621d3592ddce25e61c24a9384c21b26374ec95cc
sklionagrex-cpu <sklionagrex@gmail.com>
2026-07-31 18:56:09 +0300

    Исправлена синтаксическая ошибка f-строки в join_room

--- app.py
+++ app.py
@@ -656,7 +656,7 @@ def handle_send_message(data):
 
 @socketio.on('join_chat')
 def handle_join_chat(data):
-    join_room(f'chat_{data.get("chat_id")}')')
+    join_room(f'chat_{data.get("chat_id")}')
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
